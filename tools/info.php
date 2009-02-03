e2568645557a683dc0aaa8096835addf0b8f1ccf
estepper <estepper>
2009-02-03 08:28:29 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

---
+++ tools/info.php
@@ -0,0 +1,7 @@
+<?php
+echo '<table border="1">';
+echo '<tr><td>Server Root</td><td>' . $_SERVER['SERVER_ROOT'] . '</td></tr>';
+echo '<tr><td>Document Root</td><td>' . $_SERVER['DOCUMENT_ROOT'] . '</td></tr>';
+echo '<tr><td>Self</td><td>' . $_SERVER['PHP_SELF'] . '</td></tr>';
+echo '</table>';
+?>
