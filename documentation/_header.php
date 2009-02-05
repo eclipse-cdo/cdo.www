19aa563b4c34cd5ff7920846ca4fa413613b5b9a
estepper <estepper>
2009-02-05 16:40:01 +0000

    [154017]  Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

--- documentation/_header.php
+++ documentation/_header.php
@@ -3,8 +3,8 @@
 
 $navIconURL = "http://dev.eclipse.org/huge_icons/mimetypes/x-office-book.png";
 $Nav->addNavSeparator($areaTitle, ".");
-$Nav->addCustomNav("Version 1.0", "#version1", "", 1);
-$Nav->addCustomNav("Version 2.0", "#version2", "", 1);
+$Nav->addCustomNav("2.0 Preview", "2.0", "", 1);
+$Nav->addCustomNav("1.0 Release", "1.0", "", 1);
 $Nav->addCustomNav("Tutorials", "#tutorials", "", 1);
 $Nav->addCustomNav("Presentations", "#presentations", "", 1);
 
