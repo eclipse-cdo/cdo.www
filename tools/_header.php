302bf09fedd1a7006bf9dc71b27db0dac0724df2
estepper <estepper>
2009-02-07 08:28:21 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

--- tools/_header.php
+++ tools/_header.php
@@ -6,5 +6,21 @@ $Nav->addNavSeparator($areaTitle, "$areaPath/");
 $Nav->addCustomNav("Tables", "$areaPath/tables.php", "", 1);
 $Nav->addCustomNav("Test Forms", "$areaPath/test-forms.php", "", 1);
 
+$Nav->addNavSeparator("Test Scripts", "");
+$testRoot = "$areaRoot/test";
+if (($dir = opendir($testRoot)))
+{
+	while (($entry = readdir($dir)) !== false)
+	{
+		$file = "$testRoot/$entry";
+		if (is_file($file))
+		{
+			$Nav->addCustomNav($entry, "$areaPath/test/$entry", "", 1);
+		}
+	}
+
+	closedir($dir);
+}
+
 ########################################################################
 ?>
