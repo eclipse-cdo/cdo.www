acd6063a8266649f30216edc1cc8adbf0cd0cf65
estepper <estepper>
2009-02-12 23:42:28 +0000

    [154017]  Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

---
+++ documentation/presentations/index.php
@@ -0,0 +1,13 @@
+<?php $areaRelative = ".."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php";
+########################################################################
+
+$pageTitle 		= "Presentations";
+//$pageKeywords	= "";
+$pageAuthor		= "Eike Stepper";
+
+print '<div id="midcolumn">';
+
+print '</div>';
+
+########################################################################
+include "$areaRoot/_footer.php"; ?>
