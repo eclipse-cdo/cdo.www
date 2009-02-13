b4bb92473df0243c2260bdce813db103b6323504
estepper <estepper>
2009-02-13 21:46:25 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

--- documentation/presentations/index.php
+++ documentation/presentations/index.php
@@ -5,17 +5,75 @@ $pageTitle 		= "Presentations";
 //$pageKeywords	= "";
 $pageAuthor		= "Eike Stepper";
 
-function printPresentation($basePath, $title, $subtitle, $month, $lang = "english")
-{
-	print "<h6 class=\"homeitem\">$title\n";
-}
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="' . $pageFolderPath . '/styles.css" media="screen"/>' . "\n\t");
 
-print '<div id="midcolumn">';
+print '<div id="midcolumn">' . "\n";
+print '<h1>Presentations</h1>' . "\n";
+
+printPresentation("Webinar_2009_01/CDO_Webinar_2009_01",
+									"Scale, Share and Store your Models with CDO 2.0",
+									"Slides of the Eclipse Live <a href=\"http://live.eclipse.org/node/635\">webinar</a>.",
+									"January 2009");
+
+printPresentation("DemoCampBerlin_2008/CDO@DemoCamp_08",
+									"CDO 2.0 at the Eclipse Demo Camp in Berlin",
+									"Very brief slides, just to prepare a live demo.",
+									"November 2008");
+
+printPresentation("EclipseSummit_2008/CDO@ESE08",
+									"CDO 2.0 at the Eclipse Summit Europe 2008",
+									"Long talk: An update, code snippets with the new API...",
+									"November 2008");
+
 printPresentation("Bombardier_2008/CDO_Preview_Bombardier_2008",
 									"CDO 2.0 Preview for Bombardier",
-									"",
+									"Project plan, project themes, plan items...",
 									"October 2008");
-print '</div>';
+
+printPresentation("NASA_2008/CDO@NASA",
+									"CDO for the NASA Ames Research Center",
+									"Long talk at the annual Ensemble Conference",
+									"August 2008");
+
+printPresentation("NASA_2008/Net4j@NASA",
+									"Net4j for the NASA Ames Research Center",
+									"Short talk at the annual Ensemble Conference",
+									"August 2008");
+
+printPresentation("EclipseCon_2008/CDO-Presentation",
+									"CDO 1.0 at the EclipseCon 2008",
+									"Long talk: Introduction, design, screenshots...",
+									"March 2008");
+
+printPresentation("EclipseCon_2008/CDO-Presentation",
+									"Net4j 1.0 at the EclipseCon 2008",
+									"Short talk: Introduction, design, screenshots...",
+									"March 2008");
+
+print '</div>' . "\n";
+
+function printPresentation($basePath, $title, $subtitle, $month, $lang = "English")
+{
+	global $pageRoot;
+	$types = array(
+	array("pdf", "Adobe Acrobat Document (.pdf)"),
+	array("pptx", "Microsoft Office PowerPoint-Presentation (.pptx)"),
+	array("ppt", "Microsoft Office PowerPoint 97-2003-Presentation (.ppt)"));
+
+	print "<div class=\"titlerow\"><h6>$title</h6></div>\n";
+	print "<div class=\"detailrow\"><div class=\"iconcol\">\n";
+
+	foreach ($types as $type)
+	{
+		$file = $basePath . "." . $type[0];
+		if (file_exists("$pageRoot/$file"))
+		{
+			print "<a href=\"$file\" title=\"" . $type[1] . "\"><img class=\"icon\" src=\"" . $type[0] . ".gif\"/></a>&nbsp;\n";
+		}
+	}
+
+	print "</div><div class=\"infocol\">$subtitle<br/>$month<br/>$lang</div></div>\n";
+}
 
 ########################################################################
 include "$areaRoot/_footer.php"; ?>
