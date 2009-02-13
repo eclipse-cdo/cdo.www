e37472d0aa7e7d2916ec836b127ff9589118ae36
estepper <estepper>
2009-02-13 23:50:37 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

--- documentation/presentations/index.php
+++ documentation/presentations/index.php
@@ -13,52 +13,61 @@ print '<h1 id="pagetitle">Presentations</h1>' . "\n";
 printPresentation("Webinar_2009_01/CDO_Webinar_2009_01",
 									"Scale, Share and Store your Models with CDO 2.0",
 									"Slides of the Eclipse Live webinar.",
+									"Ed Merks and Eike Stepper",
 									"January 2009");
 
 printPresentation("DemoCampBerlin_2008/CDO@DemoCamp_08",
 									"CDO 2.0 at the Eclipse Demo Camp in Berlin",
 									"Very brief slides, just to prepare a live demo.",
+									"Eike Stepper",
 									"November 2008");
 
 printPresentation("EclipseSummit_2008/CDO@ESE08",
 									"CDO 2.0 at the Eclipse Summit Europe 2008",
 									"Long talk: An update, code snippets with the new API...",
+									"Eike Stepper",
 									"November 2008");
 
 printPresentation("https://sap.emea.pgiconnect.com/p27383431|Webinar Recording",
 									"CDO 2.0 Webinar for SAP",
 									"Slides, live presentation, discussion...",
+									"Eike Stepper",
 									"November 2008",
 									"German");
 
 printPresentation("Bombardier_2008/CDO_Preview_Bombardier_2008",
 									"CDO 2.0 Preview for Bombardier",
 									"Project plan, project themes, plan items...",
+									"Eike Stepper",
 									"October 2008");
 
 printPresentation("NASA_2008/CDO@NASA",
 									"CDO for the NASA Ames Research Center",
 									"Long talk at the annual Ensemble Conference",
+									"Eike Stepper",
 									"August 2008");
 
 printPresentation("NASA_2008/Net4j@NASA",
 									"Net4j for the NASA Ames Research Center",
 									"Short talk at the annual Ensemble Conference",
+									"Eike Stepper",
 									"August 2008");
 
 printPresentation("EclipseCon_2008/CDO-Presentation",
 									"CDO 1.0 at the EclipseCon 2008",
 									"Long talk: Introduction, design, screenshots...",
+									"Eike Stepper",
 									"March 2008");
 
 printPresentation("EclipseCon_2008/CDO-Presentation",
 									"Net4j 1.0 at the EclipseCon 2008",
 									"Short talk: Introduction, design, screenshots...",
+									"Eike Stepper",
 									"March 2008");
 
 print '</div>' . "\n";
 
-function printPresentation($basePath, $title, $subtitle, $month, $lang = "English")
+function printPresentation($basePath, $title, $subtitle, $presenters, $month, $lang = "English")
 {
 	print "<div class=\"titlerow\"><h6>$title</h6></div>\n";
 	print "<div class=\"detailrow\"><div class=\"iconcol\">\n";
@@ -107,7 +116,7 @@ function printPresentation($basePath, $title, $subtitle, $month, $lang = "Englis
 		}
 	}
 
-	print "</div><div class=\"infocol\">$subtitle<br/>$month<br/>$lang</div></div>\n";
+	print "</div><div class=\"infocol\">$subtitle<br/>$month<br/>$presenters<br/>$lang</div></div>\n";
 }
 
 ########################################################################
