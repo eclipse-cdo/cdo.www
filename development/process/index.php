654a5d363127b50ed459fde1ab8bfe18ce2ee270
estepper <estepper>
2009-09-27 19:21:52 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

--- development/process/index.php
+++ development/process/index.php
@@ -11,5 +11,114 @@ print '<h1 id="pagetitle">' . $pageTitle . '</h1>' . "\n";
 print '<p><img src="CDO-Process.png" usemap="#CDOProcess"></p>' . "\n";
 include "$pageRoot/CDO-Process.imagemap";
 
+$fieldState = "State";
+$fieldFeedback = "Feedback";
+$fieldResolution = "Resolution";
+
+$stateStart = new State("start");
+$stateNew   =	new State("new");
+$stateFeedbackN =	new State("feedback-n");
+$stateDuplicate =	new State("duplicate");
+$stateWorks =	new State("works");
+$stateNotEclipse =	new State("noteclipse");
+$stateTriaged =	new State("triaged");
+$stateFeedbackT =	new State("feedback-t");
+$stateDevelop =	new State("develop");
+$stateFeedbackD =	new State("feedback-d");
+$stateReview =	new State("review");
+$stateFeedbackR =	new State("feedback-r");
+$stateReviewed =	new State("reviewed");
+$stateFixed =	new State("fixed");
+$stateReleased =	new State("released");
+$stateClosed =	new State("closed");
+
+$stateStart->addTransition("Submit new bugzilla", $stateNew);
+$stateNew->addTransition("Get feedback from reporter", $stateFeedbackN);
+$stateNew->addTransition("Confirm", $stateTriaged);
+$stateNew->addTransition("Resolve as DUPLICATE", $stateDuplicate);
+$stateNew->addTransition("Resolve as WORKS", $stateWorks);
+$stateNew->addTransition("Resolve as NOTECLIPSE", $stateNotEclipse);
+$stateFeedbackN->addTransition("Return to team", $stateNew);
+$stateDuplicate->addTransition("Close", $stateClosed);
+$stateWorks->addTransition("Close", $stateClosed);
+$stateNotEclipse->addTransition("Close", $stateClosed);
+$stateTriaged->addTransition("Get feedback from reporter", $stateFeedbackT);
+$stateTriaged->addTransition("Start development", $stateDevelop);
+$stateFeedbackT->addTransition("Return to team", $stateTriaged);
+$stateDevelop->addTransition("Get feedback from reporter", $stateFeedbackD);
+$stateDevelop->addTransition("Stop development", $stateTriaged);
+$stateDevelop->addTransition("Request review", $stateReview);
+$stateFeedbackD->addTransition("Return to team", $stateDevelop);
+$stateReview->addTransition("Get feedback from reporter", $stateFeedbackR);
+$stateReview->addTransition("Reject changes", $stateDevelop);
+$stateReview->addTransition("Approve changes", $stateReviewed);
+$stateFeedbackR->addTransition("Return to team", $stateReview);
+$stateReviewed->addTransition("Resolve as FIXED", $stateFixed);
+$stateFixed->addTransition("Release", $stateReleased);
+$stateReleased->addTransition("Close", $stateClosed);
+
+
+$states = array(
+$stateStart,
+$stateNew,
+$stateFeedbackN,
+$stateDuplicate,
+$stateWorks,
+$stateNotEclipse,
+$stateTriaged,
+$stateFeedbackT,
+$stateDevelop,
+$stateFeedbackD,
+$stateReview,
+$stateFeedbackR,
+$stateReviewed,
+$stateFixed,
+$stateReleased,
+$stateClosed,
+);
+
+class State
+{
+	public $name;
+	public $transitions = array();
+
+	function __construct($name)
+	{
+		$this->name = $name;
+	}
+
+	function addTransition($name, $result)
+	{
+		$this->transitions[count($this->transitions)] = new Transition($name, $result);
+	}
+
+	function render()
+	{
+		print '<tr><td><a name="' . $this->name . '"/><img src="images/' . $this->name . '.png"/></td><td/></tr>' . "\n";
+		foreach ($this->transitions as $transition)
+		{
+			$transition->render();
+		}
+	}
+}
+
+class Transition
+{
+	public $name;
+	public $result;
+
+	function __construct($name, $result)
+	{
+		$this->name = $name;
+		$this->result = $result;
+	}
+
+	function render()
+	{
+		print '<tr><td><img src="images/transition.png"/><img src="images/' . $this->result->name . '.png" href="#' . $this->result->name . '"/></td><td>' . $this->name . '</td></tr>' . "\n";
+	}
+}
+
+
 ########################################################################
 include "$areaRoot/_footer.php"; ?>
