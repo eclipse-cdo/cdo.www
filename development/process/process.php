57691a0d29ce07f7b788a715b973c4cd768a4f61
estepper <estepper>
2009-09-27 17:56:16 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

---
+++ development/process/process.php
@@ -0,0 +1,14 @@
+<?php $areaRelative = ".."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php";
+########################################################################
+
+$pageTitle 		= "CDO Bugzilla Process";
+//$pageKeywords	= "";
+$pageAuthor		= "Eike Stepper";
+
+print '<div id="midcolumn">' . "\n";
+print '<h1 id="pagetitle">' . $pageTitle . '</h1>' . "\n";
+
+print '<p><img src="CDO-Process.png"></p>' . "\n";
+
+########################################################################
+include "$areaRoot/_footer.php"; ?>
