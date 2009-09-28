3f3c822d49ba60b48d0ef6c06e769f12a79d9b5c
estepper <estepper>
2009-09-28 14:19:45 +0000

    [154017] Create CDO homepage https://bugs.eclipse.org/bugs/show_bug.cgi?id=154017

--- development/process/index.php
+++ development/process/index.php
@@ -10,29 +10,35 @@ $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="' . $page
 print '<div id="midcolumn">' . "\n";
 print '<h1 id="pagetitle">' . $pageTitle . '</h1>' . "\n";
 
-$fieldState = "State";
-$fieldFeedback = "Feedback";
-$fieldResolution = "Resolution";
+$process = new Process("CDO-Process");
 
-$stateStart = new State("start");
-$stateNew   =	new State("new");
-$stateFeedbackN =	new State("feedback-n");
-$stateDuplicate =	new State("duplicate");
-$stateWorks =	new State("works");
-$stateNotEclipse =	new State("noteclipse");
-$stateTriaged =	new State("triaged");
-$stateFeedbackT =	new State("feedback-t");
-$stateDevelop =	new State("develop");
-$stateFeedbackD =	new State("feedback-d");
-$stateReview =	new State("review");
-$stateFeedbackR =	new State("feedback-r");
-$stateReviewed =	new State("reviewed");
-$stateFixed =	new State("fixed");
-$stateReleased =	new State("released");
-$stateClosed =	new State("closed");
+$stateStart = $process->addState("start", "263,13,13", "circle");
+$stateNew   =	$process->addState("new", "195,60,331,115");
+$stateFeedbackN =	$process->addState("feedback-n", "1,60,136,114");
+$stateDuplicate =	$process->addState("duplicate", "435,60,571,115");
+$stateWorks =	$process->addState("works", "435,143,572,197");
+$stateNotEclipse =	$process->addState("noteclipse", "435,225,571,279");
+$stateTriaged =	$process->addState("triaged", "196,143,331,197");
+$stateFeedbackT =	$process->addState("feedback-t", "1,143,136,197");
+$stateDevelop =	$process->addState("develop", "195,225,331,279");
+$stateFeedbackD =	$process->addState("feedback-d", "0,225,137,279");
+$stateReview =	$process->addState("review", "195,308,331,361");
+$stateFeedbackR =	$process->addState("feedback-r", "0,308,136,362");
+$stateReviewed =	$process->addState("reviewed", "196,391,331,444");
+$stateFixed =	$process->addState("fixed", "435,391,572,445");
+$stateReleased =	$process->addState("released", "435,488,571,543");
+$stateClosed =	$process->addState("closed", "645,488,782,542");
 
-$stateStart->addTransition("Submit new bugzilla", $stateNew);
-$stateNew->addTransition("Get feedback from reporter", $stateFeedbackN);
+$t1 = $stateStart->addTransition("Submit new bugzilla", $stateNew);
+$t1->addAction("<b>Project</b> = EMF");
+$t1->addAction("<b>Component</b> = CDO");
+$t1->addAction("<b>Description</b> = ");
+
+$t2 = $stateNew->addTransition("Get feedback from reporter", $stateFeedbackN);
+$t2->addAction("<b>Project</b> = EMF");
+$t2->addAction("<b>Component</b> = CDO");
+$t2->addAction("<b>Description</b> = ");
+
 $stateNew->addTransition("Confirm", $stateTriaged);
 $stateNew->addTransition("Resolve as DUPLICATE", $stateDuplicate);
 $stateNew->addTransition("Resolve as WORKS", $stateWorks);
@@ -56,41 +62,14 @@ $stateReviewed->addTransition("Resolve as FIXED", $stateFixed);
 $stateFixed->addTransition("Release", $stateReleased);
 $stateReleased->addTransition("Close", $stateClosed);
 
-
-$states = array(
-$stateStart,
-$stateNew,
-$stateFeedbackN,
-$stateDuplicate,
-$stateWorks,
-$stateNotEclipse,
-$stateTriaged,
-$stateFeedbackT,
-$stateDevelop,
-$stateFeedbackD,
-$stateReview,
-$stateFeedbackR,
-$stateReviewed,
-$stateFixed,
-$stateReleased,
-$stateClosed,
-);
-
-print '<p><img src="CDO-Process.png" usemap="#CDOProcess"></p>' . "\n";
-include "$pageRoot/CDO-Process.imagemap";
-print "<br><br><br><h1 id=\"pagetitle\">States and Transitions</h1>\n";
-
-foreach ($states as $state)
-{
-	$state->render();
-}
+$process->render();
 
 
-class State
+class Process
 {
 	public $name;
 	public $description;
-	public $transitions = array();
+	public $states = array();
 
 	function __construct($name, $description="")
 	{
@@ -98,24 +77,65 @@ class State
 		$this->description = $description;
 	}
 
-	function addTransition($name, $result)
+	function  addState($name, $coords=NULL, $shape="rect")
 	{
-		$this->transitions[count($this->transitions)] = new Transition($name, $result);
+		return $this->states[count($this->states)] = new State($name, $coords, $shape);
 	}
 
 	function render()
 	{
-		print '<div class="box">' . "\n";
-		print '  <div class="state">' . "\n";
-		print '    <div class="statename"><a name="' . $this->name . '"/><img src="images/' . $this->name . '.png"/></div>' . "\n";
-		print '    <div class="statedesc">Bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.</div>' . "\n";
-		print '  </div>' . "\n";
+		print '<p><img src="' . $this->name . '.png" usemap="#' . $this->name . '"></p>' . "\n";
+		print '<map name="' . $this->name . '">' . "\n";
+		foreach ($this->states as $state)
+		{
+			print '  <area shape="' . $state->shape . '" coords="' . $state->coords . '" href="#' . $state->name . '"  />' . "\n";
+		}
+
+		print '</map>' . "\n";
+		print "<br><br><br>\n";
+		foreach ($this->states as $state)
+		{
+			$state->render();
+		}
+	}
+}
+
+class State
+{
+	public $name;
+	public $shape;
+	public $coords;
+	public $description;
+	public $transitions = array();
+
+	function __construct($name, $coords=NULL, $shape="rect")
+	{
+		$this->name = $name;
+		$this->coords = $coords;
+		$this->shape = $shape;
+	}
+
+	function addTransition($name, $result)
+	{
+		return $this->transitions[count($this->transitions)] = new Transition($name, $result);
+	}
+
+	function render()
+	{
+		//		print '<div><a name="' . $this->name . '"/></div>' . "\n";
+		print '<div class="box" id="' . $this->name . '">' . "\n";
+		print '  <table class="state"><tr>' . "\n";
+		print '    <td class="statename" valign="top" width="190"><img src="images/' . $this->name . '.png"/></td>' . "\n";
+		print '    <td class="statedesc" valign="top">' . $this->description . '</td>' . "\n";
+		print '  </tr></table>' . "\n";
+		print '  <div class="transitions">' . "\n";
 
 		foreach ($this->transitions as $transition)
 		{
 			$transition->render($this);
 		}
 
+		print '  </div>' . "\n";
 		print '</div>' . "\n";
 	}
 }
@@ -125,6 +145,7 @@ class Transition
 	public $name;
 	public $result;
 	public $description;
+	public $actions = array();
 
 	function __construct($name, $result, $description="")
 	{
@@ -133,18 +154,32 @@ class Transition
 		$this->description = $description;
 	}
 
+	function addAction($action)
+	{
+		return $this->actions[count($this->actions)] = $action;
+	}
+
 	function render($current)
 	{
-		print '  <div class="trans">' . "\n";
-		print '    <div class="transpic">' . "\n";
-		print '      <a href="#' . $current->name . '"><img src="images/' . $current->name . '.png"/></a>';
+		print '    <table class="trans"><tr>' . "\n";
+		print '      <td class="transpic" valign="top" width="380">' . "\n";
+		print '        <a href="#' . $current->name . '"><img src="images/' . $current->name . '.png"/></a>';
 		print ($current->name == "start") ? '<img src="images/line.png"/>' : "";
 		print '<img src="images/transition.png"/>';
 		print '<a href="#' . $this->result->name . '"><img src="images/' . $this->result->name . '.png"/></a>' . "\n";
-		print '    </div>' . "\n";
-		print '    <div class="transname">' . $this->name . '</div>' . "\n";
-		print '    <div class="transdesc">' . $this->description . '</div>' . "\n";
-		print '  </div>' . "\n";
+		print '      </td>' . "\n";
+		print '      <td valign="top"><br>' . "\n";
+		print '        <h3 class="transname">' . $this->name . '</h3>' . "\n";
+		print '        <p class="transdesc">' . $this->description . '</p>' . "\n";
+		print '        <ul>' . "\n";
+		foreach ($this->actions as $action)
+		{
+			print '          <li>' . "$action\n";
+		}
+
+		print '        </ul>' . "\n";
+		print '      </td>' . "\n";
+		print '    </tr></table>' . "\n";
 	}
 }
 
