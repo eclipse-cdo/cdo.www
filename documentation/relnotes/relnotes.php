15a571eac91ca47a5f5802e2e333e65bd271e8f6
estepper <estepper>
2010-05-08 10:55:58 +0000

    *** empty log message ***

---
+++ documentation/relnotes/relnotes.php
@@ -0,0 +1,11 @@
+<?php
+
+function printReleaseNotes($url)
+{
+	$region = 0;
+	$content = file_get_contents($url);
+	preg_match('<body>(.*)</body>', $content, $matches);
+	print "$matches[0]\n";
+}
+
+?>
