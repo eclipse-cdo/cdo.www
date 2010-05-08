cead7d18c19fa7aec5c06349635475107a03b5f8
estepper <estepper>
2010-05-08 11:30:11 +0000

    *** empty log message ***

--- documentation/relnotes.php
+++ documentation/relnotes.php
@@ -4,9 +4,7 @@ function printReleaseNotes($url)
 {
 	$content = file_get_contents($url);
 	ereg('.*<body>(.*)</body>.*', $content, $matches);
-	print "7: $matches[1]\n";
-	print "<p> $matches\n";
-	// print "<p> $content\n";
+	print "$matches[1]\n";
 }
 
 ?>
