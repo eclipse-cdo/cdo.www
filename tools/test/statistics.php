dad7a2f05d7a990973c53a8400cc914a7a3e522e
mfluegge <mfluegge>
2010-08-03 19:39:36 +0000

    NEW - bug 320196: Committer statistics out of sync since net4j migration https://bugs.eclipse.org/bugs/show_bug.cgi?id=320196

--- tools/test/statistics.php
+++ tools/test/statistics.php
@@ -105,7 +105,7 @@ if ($result && mysql_num_rows($result) > 0)
 			"MAX(date) AS UntilDate " . 
 			"FROM commits JOIN bugs JOIN bugdescs JOIN cvsfiles " . 
 			"WHERE Author = '" . $author . "' AND commits.fid = bugs.fid AND commits.revision = bugs.revision AND bugs.bugid = bugdescs.bugid AND commits.fid = cvsfiles.fid AND ".
-				"(cvsfiles.component LIKE 'org.eclipse.emf.cdo%' " . 
+				"cvsfiles.component LIKE 'org.eclipse.emf.cdo%' " . 
 			"GROUP BY BugID " . 
 			"ORDER BY UntilDate DESC");
 
