26186d9c89eaea5bd95ce2f501dabf27ef2009bb
mfluegge <mfluegge>
2010-08-03 19:01:28 +0000

    NEW - bug 320196: Committer statistics out of sync since net4j migration https://bugs.eclipse.org/bugs/show_bug.cgi?id=320196

--- tools/test/statistics.php
+++ tools/test/statistics.php
@@ -1,4 +1,4 @@
-<?php $areaRelative = ".."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php"; 
+<?php $areaRelative = "."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php"; 
 ########################################################################
 
 require_once "$docRoot/modeling/includes/db.php";
@@ -102,7 +102,7 @@ if ($result && mysql_num_rows($result) > 0)
 			"Title, " . 
 			"MAX(date) AS UntilDate " . 
 			"FROM commits JOIN bugs JOIN bugdescs JOIN cvsfiles " . 
-			"WHERE Author = '" . $author . "' AND commits.fid = bugs.fid AND commits.revision = bugs.revision AND bugs.bugid = bugdescs.bugid AND commits.fid = cvsfiles.fid AND (cvsfiles.component = 'org.eclipse.emf.cdo' OR cvsfiles.component = 'org.eclipse.emf.cdo.core' OR cvsfiles.component = 'org.eclipse.emf.cdo.dawn' OR cvsfiles.component = 'org.eclipse.emf.cdo.db' OR cvsfiles.component = 'org.eclipse.emf.cdo.docs' OR cvsfiles.component = 'org.eclipse.emf.cdo.hibernate' OR cvsfiles.component = 'org.eclipse.emf.cdo.net4j' OR cvsfiles.component = 'org.eclipse.emf.cdo.net4j.db' OR cvsfiles.component = 'org.eclipse.emf.cdo.net4j.ui' OR cvsfiles.component = 'org.eclipse.emf.cdo.objy' OR cvsfiles.component = 'org.eclipse.emf.cdo.releng'OR cvsfiles.component = 'org.eclipse.emf.cdo.ui') " . 
+			"WHERE Author = '" . $author . "' AND commits.fid = bugs.fid AND commits.revision = bugs.revision AND bugs.bugid = bugdescs.bugid AND commits.fid = cvsfiles.fid AND cvsfiles.component = 'org.eclipse.emf.cdo' " . 
 			"GROUP BY BugID " . 
 			"ORDER BY UntilDate DESC");
 
