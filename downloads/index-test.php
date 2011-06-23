b7e7a409a8cbd86a4a4eeef795355742b8c6a8dd
estepper <estepper>
2011-06-23 16:41:19 +0000

    *** empty log message ***

--- downloads/index-test.php
+++ downloads/index-test.php
@@ -12,7 +12,63 @@ $App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="te
 $pageAuthor		= "Eike Stepper";
 
 print '<div id="midcolumn">';
-include("http://download.eclipse.org/modeling/emf/cdo/updates/downloads.php");
+//include("http://download.eclipse.org/modeling/emf/cdo/updates/downloads.html");
+?>
+
+<h1>Releases</h1>
+<ul>
+<li><b><a href="releases">Composite Update Site</a></b></li>
+</ul>
+<h2>4.0</h2>
+<ul>
+<li><b><a href="releases/4.0">Composite Update Site</a></b></li>
+<li><b>R20110608-1407</b>&nbsp;-&nbsp;<a href="../drops/R20110608-1407">Contents</a>&nbsp;-&nbsp;<a href="../drops/R20110608-1407">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/R20110608-1407/zips/emf-cdo-R20110608-1407-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/R20110608-1407/zips/emf-cdo-R20110608-1407-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+
+</ul>
+<h2>3.0</h2>
+<ul>
+<li><b><a href="releases/3.0">Composite Update Site</a></b></li>
+</ul>
+<h2>2.0</h2>
+<ul>
+<li><b><a href="releases/2.0">Composite Update Site</a></b></li>
+</ul>
+<h1>4.1 Integration</h1>
+<ul>
+<li><b><a href="integration">Composite Update Site</a></b></li>
+
+</ul>
+<h2>Stable Builds</h2>
+<ul>
+<li><b><a href="integration/stable">Composite Update Site</a></b></li>
+</ul>
+<h2>Weekly Builds</h2>
+<ul>
+<li><b><a href="integration/weekly">Composite Update Site</a></b></li>
+<li><b>I20110622-0357</b>&nbsp;-&nbsp;<a href="../drops/I20110622-0357">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110622-0357">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0357/zips/emf-cdo-I20110622-0357-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0357/zips/emf-cdo-I20110622-0357-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+
+<li><b>I20110622-0512</b>&nbsp;-&nbsp;<a href="../drops/I20110622-0512">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110622-0512">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0512/zips/emf-cdo-I20110622-0512-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0512/zips/emf-cdo-I20110622-0512-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+<li><b>I20110622-0601</b>&nbsp;-&nbsp;<a href="../drops/I20110622-0601">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110622-0601">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0601/zips/emf-cdo-I20110622-0601-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0601/zips/emf-cdo-I20110622-0601-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+
+<li><b>I20110623-1120</b>&nbsp;-&nbsp;<a href="../drops/I20110623-1120">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110623-1120">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110623-1120/zips/emf-cdo-I20110623-1120-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110623-1120/zips/emf-cdo-I20110623-1120-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+</ul>
+<h1>4.0 Maintenance</h1>
+<ul>
+<li><b><a href="maintenance">Composite Update Site</a></b></li>
+</ul>
+
+<h2>Stable Builds</h2>
+<ul>
+<li><b><a href="maintenance/stable">Composite Update Site</a></b></li>
+</ul>
+<h2>Weekly Builds</h2>
+<ul>
+<li><b><a href="maintenance/weekly">Composite Update Site</a></b></li>
+<li><b>M20110623-1122</b>&nbsp;-&nbsp;<a href="../drops/M20110623-1122">Contents</a>&nbsp;-&nbsp;<a href="../drops/M20110623-1122">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/M20110623-1122/zips/emf-cdo-M20110623-1122-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/M20110623-1122/zips/emf-cdo-M20110623-1122-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+
+</ul>
+
+<?php
 print '</div>';
 
 
--- downloads/index.php
+++ downloads/index.php
@@ -1,6 +1,8 @@
 <?php $areaRelative = "."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php";
 ########################################################################
 
+require_once "$areaRoot/downloads.php";
+
 $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/common.css"/>' . "\n\t");
 $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n\t");
 $App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n\t");
@@ -9,48 +11,64 @@ $App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="te
 //$pageKeywords	= "";
 $pageAuthor		= "Eike Stepper";
 
-
-function printP2Repositories($branchName,  $entries)
-{
-	print "<h2>P2 repositories for ".$branchName."</h2>";
-
-	for($i=0; $i<count($entries); $i++)
-	{
-		print "<h4>".$entries[$i]['name']."</h4>";
-		print "<a href='".$entries[$i]['url']."'>".$entries[$i]['url']."</a><br><br>";
-	}
-	print '<br><br>';
-}
-
-
 print '<div id="midcolumn">';
+//include("http://download.eclipse.org/modeling/emf/cdo/updates/downloads.html");
+?>
 
-/*################################### 4.0 ##############################################*/
-$entries=array(
-array("name" => "4.0 release builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/releases/4.0"),
-array("name" => "Latest stable build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance/lastStableBuild/artifact/result/site.p2"),
-array("name" => "Latest successful build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance/lastSuccessfulBuild/artifact/result/site.p2")
-);
+<h1>Releases</h1>
+<ul>
+<li><b><a href="releases">Composite Update Site</a></b></li>
+</ul>
+<h2>4.0</h2>
+<ul>
+<li><b><a href="releases/4.0">Composite Update Site</a></b></li>
+<li><b>R20110608-1407</b>&nbsp;-&nbsp;<a href="../drops/R20110608-1407">Contents</a>&nbsp;-&nbsp;<a href="../drops/R20110608-1407">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/R20110608-1407/zips/emf-cdo-R20110608-1407-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/R20110608-1407/zips/emf-cdo-R20110608-1407-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
 
-printP2Repositories("CDO 4.0",  $entries);
+</ul>
+<h2>3.0</h2>
+<ul>
+<li><b><a href="releases/3.0">Composite Update Site</a></b></li>
+</ul>
+<h2>2.0</h2>
+<ul>
+<li><b><a href="releases/2.0">Composite Update Site</a></b></li>
+</ul>
+<h1>4.1 Integration</h1>
+<ul>
+<li><b><a href="integration">Composite Update Site</a></b></li>
 
-/*################################### 3.0 ##############################################*/
-$entries=array(
-array("name" => "3.0 release builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/releases/3.0"),
-);
+</ul>
+<h2>Stable Builds</h2>
+<ul>
+<li><b><a href="integration/stable">Composite Update Site</a></b></li>
+</ul>
+<h2>Weekly Builds</h2>
+<ul>
+<li><b><a href="integration/weekly">Composite Update Site</a></b></li>
+<li><b>I20110622-0357</b>&nbsp;-&nbsp;<a href="../drops/I20110622-0357">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110622-0357">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0357/zips/emf-cdo-I20110622-0357-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0357/zips/emf-cdo-I20110622-0357-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
 
-printP2Repositories("CDO 3.0",  $entries);
+<li><b>I20110622-0512</b>&nbsp;-&nbsp;<a href="../drops/I20110622-0512">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110622-0512">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0512/zips/emf-cdo-I20110622-0512-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0512/zips/emf-cdo-I20110622-0512-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+<li><b>I20110622-0601</b>&nbsp;-&nbsp;<a href="../drops/I20110622-0601">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110622-0601">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0601/zips/emf-cdo-I20110622-0601-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110622-0601/zips/emf-cdo-I20110622-0601-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
 
-/*################################### HEAD #######################################################*/
-$entries=array(
-array("name" => "4.1 milestone builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/integration/stable"),
-array("name" => "4.1 integration builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/integration/weekly"),
-array("name" => "Latest stable build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-integration\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-integration/lastStableBuild/artifact/site.p2"),
-array("name" => "Latest successful build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-integration\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-integration/lastSuccessfulBuild/artifact/site.p2")
-);
+<li><b>I20110623-1120</b>&nbsp;-&nbsp;<a href="../drops/I20110623-1120">Contents</a>&nbsp;-&nbsp;<a href="../drops/I20110623-1120">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110623-1120/zips/emf-cdo-I20110623-1120-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/I20110623-1120/zips/emf-cdo-I20110623-1120-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
+</ul>
+<h1>4.0 Maintenance</h1>
+<ul>
+<li><b><a href="maintenance">Composite Update Site</a></b></li>
+</ul>
 
-printP2Repositories("CDO 4.1 Preview",  $entries);
+<h2>Stable Builds</h2>
+<ul>
+<li><b><a href="maintenance/stable">Composite Update Site</a></b></li>
+</ul>
+<h2>Weekly Builds</h2>
+<ul>
+<li><b><a href="maintenance/weekly">Composite Update Site</a></b></li>
+<li><b>M20110623-1122</b>&nbsp;-&nbsp;<a href="../drops/M20110623-1122">Contents</a>&nbsp;-&nbsp;<a href="../drops/M20110623-1122">Update Site</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/M20110623-1122/zips/emf-cdo-M20110623-1122-Site.zip&amp;protocol=http&amp;format=xml">Update Site Archive</a>&nbsp;-&nbsp;<a href="http://www.eclipse.org/downloads/download.php?file=/modeling/emf/cdo/drops/M20110623-1122/zips/emf-cdo-M20110623-1122-All.zip&amp;protocol=http&amp;format=xml">Dropins Archive</a></li>
 
+</ul>
+
+<?php
 print '</div>';
 
 
