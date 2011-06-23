21ce57e0e6e97c55000b8df60ec5741e0a527ce0
estepper <estepper>
2011-06-23 15:35:34 +0000

    *** empty log message ***

--- downloads/updates.php
+++ downloads/updates.php
@@ -13,24 +13,31 @@ $pageAuthor		= "Martin Fluegge";
 
 print '<div id="midcolumn">';
 
-/*################################### 3.0 ##############################################*/
+/*################################### 4.0 ##############################################*/
 $entries=array(
-array("name" => "3.0 release builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/3.0-releases"),
+array("name" => "4.0 release builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/releases/4.0"),
 array("name" => "Latest stable build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance/lastStableBuild/artifact/result/site.p2"),
 array("name" => "Latest successful build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-maintenance/lastSuccessfulBuild/artifact/result/site.p2")
+);
+
+printP2Repositories("CDO 4.0",  $entries);
+
+/*################################### 3.0 ##############################################*/
+$entries=array(
+array("name" => "3.0 release builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/releases/3.0"),
 );
 
 printP2Repositories("CDO 3.0",  $entries);
 
 /*################################### HEAD #######################################################*/
 $entries=array(
-array("name" => "4.0 milestone builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/4.0-milestones"),
-array("name" => "4.0 integration builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/4.0-integration"),
+array("name" => "4.1 milestone builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/integration/stable"),
+array("name" => "4.1 integration builds", "url" => "http://download.eclipse.org/modeling/emf/cdo/updates/integration/weekly"),
 array("name" => "Latest stable build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-integration\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-integration/lastStableBuild/artifact/site.p2"),
 array("name" => "Latest successful build (<a href=\"https://hudson.eclipse.org/hudson/job/emf-cdo-integration\">continuous integration</a>)", "url" => "https://hudson.eclipse.org/hudson/job/emf-cdo-integration/lastSuccessfulBuild/artifact/site.p2")
 );
 
-printP2Repositories("CDO 4.0 Preview",  $entries);
+printP2Repositories("CDO 4.1 Preview",  $entries);
 
 print '</div>';
 
