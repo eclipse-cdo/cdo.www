6339ecccf879a738b1dd1dc2c21118ee24220407
estepper <estepper>
2011-06-23 17:37:44 +0000

    *** empty log message ***

--- downloads/_header.php
+++ downloads/_header.php
@@ -6,7 +6,6 @@ $Nav->addNavSeparator($areaTitle, "");
 $Nav->addCustomNav("Releases", "$areaPath/index.php", "", 1);
 $Nav->addCustomNav("4.1 Integration", "$areaPath/index.php#integration", "", 1);
 $Nav->addCustomNav("4.0 Maintenance", "$areaPath/index.php#maintenance", "", 1);
-$Nav->addCustomNav("Release Notes", "$areaPath/../documentation/relnotes_30/index.php", "", 1);
 $Nav->addCustomNav("License", "http://www.eclipse.org/org/documents/epl-v10.php", "", 1);
 
 ########################################################################
