5ceb79327f882aa2df7610a75a0a7031c1e332cc
estepper <estepper>
2012-06-21 06:20:59 +0000

    [383170] [Releng] Create index.html files for composite repositories https://bugs.eclipse.org/bugs/show_bug.cgi?id=383170

--- downloads/index.php
+++ downloads/index.php
@@ -1,8 +1,8 @@
 <?php $areaRelative = "."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php";
 ########################################################################
 
-$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/common.css"/>' . "\n\t");
-$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n\t");
+//$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/common.css"/>' . "\n\t");
+//$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n\t");
 $App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n\t");
 
 //$pageTitle 		= "";
