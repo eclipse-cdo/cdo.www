4c68137c5cecfd21310252dcb69825bbce1269da
estepper <estepper>
2012-07-12 09:53:01 +0000

    Update help to 4.1

--- documentation/_header.php
+++ documentation/_header.php
@@ -8,32 +8,18 @@ $Nav->addCustomNav("4.0 Release Help", "http://help.eclipse.org/indigo/index.jsp
 
 $qualifiers = file_get_contents("http://download.eclipse.org/modeling/emf/cdo/updates/latest.qualifiers");
 
-/*
-if (preg_match('@releases_4_0 = (.*)@', $qualifiers, $match))
+if (preg_match('@releases_4_1 = (.*)@', $qualifiers, $match))
 {
-	$latest40 = $match[1];
+	$latest41 = $match[1];
+	$Nav->addCustomNav("4.1 Release Help", "http://download.eclipse.org/modeling/emf/cdo/drops/" . $latest41 . "/help", "", 1);
 }
-
-if (preg_match('@releases_3_0 = (.*)@', $qualifiers, $match))
-{
-	$latest30 = $match[1];
-	$Nav->addCustomNav("3.0 Release Help", "http://download.eclipse.org/modeling/emf/cdo/drops/" . $latest30 . "/help", "", 1);
-}
-*/
 
 if (preg_match('@integration = (.*)@', $qualifiers, $match))
 {
 	$latestIntegration = $match[1];
-	$Nav->addCustomNav("4.1 Preview Help", "http://download.eclipse.org/modeling/emf/cdo/drops/" . $latestIntegration . "/help", "", 1);
+	$Nav->addCustomNav("4.2 Preview Help", "http://download.eclipse.org/modeling/emf/cdo/drops/" . $latestIntegration . "/help", "", 1);
 }
 
-/*
-if (preg_match('@maintenance = (.*)@', $qualifiers, $match))
-{
-	$latestMaintenance = $match[1];
-	$Nav->addCustomNav("Maintenance Help", "http://download.eclipse.org/modeling/emf/cdo/drops/" . $latestMaintenance . "/help", "", 1);
-}
-*/
 $Nav->addCustomNav("Presentations", "$areaPath/presentations", "", 1);
 
 ########################################################################
