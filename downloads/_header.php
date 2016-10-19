1b9ad0a1112f6efac0c9399c257c6d40ffd1f83e
Eike Stepper <stepper@esc-net.de>
2016-10-19 12:27:15 +0200

    New homepage

--- downloads/_header.php
+++ downloads/_header.php
@@ -3,9 +3,9 @@
 
 $navIconURL = "http://dev.eclipse.org/huge_icons/actions/go-down.png";
 $Nav->addNavSeparator($areaTitle, "");
-$Nav->addCustomNav("Releases!", "$areaPath/index.php#releases", "", 2);
-$Nav->addCustomNav("Integration", "$areaPath/index.php#integration", "", 2);
-$Nav->addCustomNav("Maintenance", "$areaPath/index.php#maintenance", "", 2);
+$Nav->addCustomNav("Releases", "$areaPath/index.php#releases", "", 1);
+$Nav->addCustomNav("Integration", "$areaPath/index.php#integration", "", 1);
+$Nav->addCustomNav("Maintenance", "$areaPath/index.php#maintenance", "", 1);
 $Nav->addCustomNav("Browse Sources", "http://git.eclipse.org/c/cdo/cdo.git", "", 1);
 $Nav->addCustomNav("Install Sources", "http://wiki.eclipse.org/CDO_Source_Installation", "", 1);
 $Nav->addCustomNav("License", "http://www.eclipse.org/org/documents/epl-v10.php", "", 1);
