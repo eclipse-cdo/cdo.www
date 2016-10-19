97489ad86d82d298c3237a7d653e8bfab0783439
Eike Stepper <stepper@esc-net.de>
2016-10-19 08:55:53 +0200

    New homepage

--- index.php
+++ index.php
@@ -81,41 +81,45 @@ $pageAuthor		= "Eike Stepper";
 	   <tr>
 	     <td><img src="/cdo/images/Logo-CDO.png" width="160" height="100"></img></td>
 	     <td>
-          <p>CDO is both a development-time model repository and a run-time persistence framework.
-          Being highly optimized it supports object graphs of arbitrary size.
-          <p>CDO offers transactions with save points, explicit locking, change notifications, 
+          CDO is both a development-time model repository and a run-time persistence framework.
+          Being highly optimized it supports object graphs of arbitrary size.<br>
+          CDO offers transactions with save points, explicit locking, change notifications, 
           queries, transparent temporality, branching, merging, offline and fail-over modes, ...
        </td>
 	   </tr>
 	   <tr><td colspan="2">
-	     <center>
        <table border="0" cellspacing="20" cellpadding="20">
          <tr>
+           <td width="32px"/>
            <td><img src="https://dev.eclipse.org/huge_icons/actions/go-bottom.png"></img></td>
            <td><h3><a href="/cdo/downloads/">Download</a></h3><p><i>Looking for the latest build? Milestones, maintenance builds, and more...</i></p></td>
          </tr>
          <tr>
+           <td width="32px"/>
            <td><img src="https://dev.eclipse.org/huge_icons/mimetypes/x-office-book.png"></img></td>
            <td><h3><a href="/cdo/documentation/">Documentation</a></h3><p><i>Browse through the product documentation, tutorials, presentations and the JavaDocs...</i></p></td>
          </tr>
          <tr>
+           <td width="32px"/>
            <td><img src="https://dev.eclipse.org/huge_icons/actions/mail-reply-all.png"></img></td>
            <td><h3><a href="/cdo/support/">Support</a></h3><p><i>You have problems or questions not answered in the documentation? Look here for help...</i></p></td>
          </tr>
          <tr>
+           <td width="32px"/>
            <td><img src="https://dev.eclipse.org/huge_icons/categories/applications-internet.png"></img></td>
            <td><h3><a href="/cdo/community/">Community</a></h3><p><i>Visit the community pages for information about various product and development topics...</i></p></td>
          </tr>
          <tr>
+           <td width="32px"/>
            <td><img src="https://dev.eclipse.org/huge_icons/categories/applications-development.png"></img></td>
            <td><h3><a href="/cdo/development/">Development</a></h3><p><i>Get the sources and find out more about the CDO project and its development process...</i></p></td>
          </tr>
          <tr>
+           <td width="32px"/>
            <td><img src="https://dev.eclipse.org/huge_icons/apps/system-users.png"></img></td>
            <td><h3><a href="/cdo/team/">Team</a></h3><p><i>About us and our activity...</i></p></td>
          </tr>
        </table>
-	     </center>
 	   </td></tr>
 	 </table>
 	</div>
