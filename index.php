0e0959a82a14cd202e9992672eedac7ca9cb913a
Eike Stepper <stepper@esc-net.de>
2016-10-19 09:16:47 +0200

    New homepage

--- index.php
+++ index.php
@@ -16,11 +16,9 @@ $pageAuthor		= "Eike Stepper";
 	     <td><img src="/cdo/images/Logo-CDO.png" width="160" height="100"></img></td>
        <td width="32px"/>
 	     <td>
-          CDO is both a development-time model repository and a run-time persistence framework.
+          <p>CDO is both a development-time model repository and a run-time persistence framework.
           Being highly optimized it supports object graphs of arbitrary size.
-          <br>
-          <br>
-          CDO offers transactions with save points, explicit locking, change notifications, 
+          <p>CDO offers transactions with save points, explicit locking, change notifications, 
           queries, transparent temporality, time travel, branching, merging, offline clones,
           fail-over clusters, automatic memory management, and more...
        </td>
