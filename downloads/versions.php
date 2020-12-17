b0056be3a3da020dff19df968c9f64608751ef35
Eike Stepper <stepper@esc-net.de>
2020-12-17 12:58:21 +0100

    Version matrix
    Signed-off-by: Eike Stepper <stepper@esc-net.de>

--- downloads/versions.php
+++ downloads/versions.php
@@ -1,4 +1,5 @@
-<?php $areaRelative = "."; require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php";
+<?php $areaRelative = "."; 
+require_once "$areaRelative/_defs.php";  include "$areaRoot/_header.php";
 ########################################################################
 
 header( 'Cache-control: no cache' );
