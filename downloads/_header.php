2a5eb60c9f225efb9fd10591f667410c862d73c6
Eike Stepper <stepper@esc-net.de>
2022-11-21 10:21:18 +0100

    Version matrix

--- downloads/_header.php
+++ downloads/_header.php
@@ -5,7 +5,7 @@ $navIconURL = "http://dev.eclipse.org/huge_icons/actions/go-down.png";
 $Nav->addNavSeparator($areaTitle, "");
 $Nav->addCustomNav("Releases", "$areaPath/index.php#releases", "", 1);
 $Nav->addCustomNav("Integration", "$areaPath/index.php#integration", "", 1);
-$Nav->addCustomNav("Compare Versions", "$areaPath/versions.php", "", 1);
+$Nav->addCustomNav("Compare Versions", "$areaPath/versions.php?max=8", "", 1);
 $Nav->addCustomNav("Browse Sources", "http://git.eclipse.org/c/cdo/cdo.git", "", 1);
 $Nav->addCustomNav("Install Sources", "http://wiki.eclipse.org/CDO_Source_Installation", "", 1);
 $Nav->addCustomNav("License", "http://www.eclipse.org/org/documents/epl-v10.php", "", 1);
